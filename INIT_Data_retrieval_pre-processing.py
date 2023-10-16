752ecba4cfbac3939f809ab5ef30e3e6310ab3cb
Marti Jimenez <marti.jimenez.2002@gmail.com>
2023-10-16 16:48:53 +0200

    added script to vectorize the output data that will be used as target of the model. The target data is saved as a numpy file "target.npy"

--- INIT_Data_retrieval_pre-processing.py
+++ INIT_Data_retrieval_pre-processing.py
@@ -6,7 +6,7 @@ import logging
 START_FROM_BEGINNING = True ### IF THIS VARIABLE IS SET TO TRUE THE PROCESSING WILL START FROM THE BEGINNING, 
                              ### ERASING ALL PREVIOUS PROGRESS
 
-# word2vec_model = api.load("word2vec-google-news-300")
+
 
 if START_FROM_BEGINNING:
     logging.basicConfig(filename='data_processing.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
@@ -16,4 +16,4 @@ if START_FROM_BEGINNING:
     with open('processed_output_data.csv', 'w', newline='', encoding='utf-8') as f:
         f.write('output\n')  # Add the header for the 'output' column
 
-print("Successfull Reset")
+print("Successfull INIT/Reset")
