0345a2e49458c5d29fa60d2209fa047912a264a6
Samuel Coste <s.f.coste@student.utwente.nl>
2023-10-18 16:37:55 +0200

    Update Petit_document_perso.py

--- Petit_document_perso.py
+++ Petit_document_perso.py
@@ -3,12 +3,13 @@ import numpy as np
 file_path = 'Data/Vectors/'
 
 # Load the Y_train vector from the file
+Y_total = np.load('Data/OutputData/O_data_chunk_250000.npy')
 Y_train = np.load(file_path + 'Y_train.npy')
 Y_test = np.load(file_path + 'Y_test.npy')
 Y_validate = np.load(file_path + 'Y_validate.npy')
 
-vectors = [Y_train, Y_test, Y_validate]
-vector_names = ['Train', 'Test', 'Validate']
+vectors = [Y_total, Y_train, Y_test, Y_validate]
+vector_names = ['Total', 'Train', 'Test', 'Validate']
 
 class_labels = ['pop', 'rap', 'others']
 
